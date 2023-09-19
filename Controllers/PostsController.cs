030010357fcd8d23ee02c1e85753337b2e177780
HardcoreMagazine <HardcoreMagazine@users.noreply.github.com>
2023-09-19 19:17:00 +0600

    Small fixes for Post views Details, Edit, added MaxLen check for page editing; enabled input "Post.Title" for Edit view for moderator/admin; allowed user to delete their post

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -12,6 +12,7 @@ namespace SelenicSparkApp.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly Converter _converter;
+        private const int MaxPostLen = 20_000;
 
         public PostsController(ApplicationDbContext context)
         {
@@ -115,14 +116,14 @@ namespace SelenicSparkApp.Controllers
             {
                 return BadRequest();
             }
-            if (post.Text?.Length > 20_000)
+            if (post.Text?.Length > MaxPostLen)
             {
                 return BadRequest();
             }
             if (ModelState.IsValid)
             {
                 post.CreatedDate = DateTimeOffset.UtcNow;
-                if (!string.IsNullOrWhiteSpace(post.Text))
+                if (!string.IsNullOrWhiteSpace(post.Text)) // Text will be processed regardless, Post.Text is nullable
                 {
                     var pipeline = new MarkdownPipelineBuilder()
                         .UseBootstrap()
@@ -156,7 +157,6 @@ namespace SelenicSparkApp.Controllers
             if (post.Author ==  User.Identity?.Name || User.IsInRole("Admin") || User.IsInRole("Moderator"))
             {
                 // Reverse-translate HTML to Markdown
-
                 post.Text = _converter.Convert(post.Text);
                 return View(post);
             }
@@ -172,7 +172,7 @@ namespace SelenicSparkApp.Controllers
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("PostId,Text")] Post partialPost)
+        public async Task<IActionResult> Edit(int id, [Bind("PostId,Title,Text")] Post partialPost)
         {
             if (id != partialPost.PostId)
             {
@@ -186,16 +186,31 @@ namespace SelenicSparkApp.Controllers
             }
             else
             {
+                // Process Title (4 < len < 300, not null)
+                if (!string.IsNullOrWhiteSpace(partialPost.Title))
+                {
+                    if (partialPost.Title != post.Title && (partialPost.Title.Length > 4 && partialPost.Title.Length < 300))
+                    {
+                        post.Title = partialPost.Title;
+                    }
+                }
+                // Process Text (len < MaxPostLen, nullable)
                 if (!string.IsNullOrWhiteSpace(partialPost.Text))
                 {
+                    // Trim if exeeds maximum length
+                    if (partialPost.Text.Length > MaxPostLen)
+                    {
+                        partialPost.Text = partialPost.Text[..MaxPostLen]; // .Substring(0, MaxPostLen);
+                    }
                     var pipeline = new MarkdownPipelineBuilder()
                         .UseBootstrap()
                         .UseEmojiAndSmiley(false)
                         .Build();
-                    post.Text = Markdown.ToHtml(partialPost.Text, pipeline);
-                    _context.Post.Update(post);
-                    await _context.SaveChangesAsync();
+                    partialPost.Text = Markdown.ToHtml(partialPost.Text, pipeline);
                 }
+                post.Text = partialPost.Text;
+                _context.Post.Update(post);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Details), new { id = partialPost.PostId });
             }
         }
@@ -204,19 +219,25 @@ namespace SelenicSparkApp.Controllers
         [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Post == null)
+            if (id == null || !_context.Post.Any())
             {
                 return NotFound();
             }
 
-            var post = await _context.Post
-                .FirstOrDefaultAsync(m => m.PostId == id);
+            var post = await _context.Post.FindAsync(id);
             if (post == null)
             {
                 return NotFound();
             }
 
-            return View(post);
+            if (post.Author == User.Identity?.Name || User.IsInRole("Admin") || User.IsInRole("Moderator"))
+            {
+                return View(post);
+            }
+            else
+            {
+                return Forbid();
+            }
         }
 
         // POST: Posts/Delete
@@ -225,18 +246,25 @@ namespace SelenicSparkApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Post == null)
+            if (!_context.Post.Any())
             {
-                return Problem("Entity set 'ApplicationDbContext.Post'  is null.");
+                return NotFound();
             }
             var post = await _context.Post.FindAsync(id);
-            if (post != null)
+            if (post == null)
+            {
+                return NotFound();
+            }
+            if (post.Author == User.Identity?.Name || User.IsInRole("Admin") || User.IsInRole("Moderator"))
             {
                 _context.Post.Remove(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            else
+            {
+                return Forbid();
+            }
         }
     }
 }
