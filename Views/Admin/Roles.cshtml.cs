62a69b1614e42a0fe487c0e860251db84511955d
HardcoreMagazine <HardcoreMagazine@users.noreply.github.com>
2023-09-20 12:40:10 +0600

    Cosmetic changes, trim code

--- Views/Admin/Roles.cshtml.cs
+++ Views/Admin/Roles.cshtml.cs
@@ -1,12 +1,9 @@
 #nullable disable
 
-using Microsoft.AspNetCore.Identity;
-
 namespace SelenicSparkApp.Views.Admin
 {
     public class RolesModel
     {
-        public IQueryable<IdentityRole> Roles { get; set; }
 		public string CreateRoleName { get; set; }
 	}
 }
